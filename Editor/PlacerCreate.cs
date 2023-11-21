0d77c29322c3487f857999784a740626c12afb77
SLGerr <maxbltslg@mail.ru>
2023-11-21 21:58:26 +0300

    Using UnityEditor fix

--- Editor/PlacerCreate.cs
+++ Editor/PlacerCreate.cs
@@ -1,14 +1,16 @@
 ﻿using Scene_LogicConstructor.Runtime;
-using UnityEditor;
 using UnityEngine;
 
 namespace Scene_LogicConstructor.Editor
 {
     public static class PlacerCreate
     {
-        [MenuItem("Tools/Scene-LogicConstructor/Add Logic Placer")]
+#if UNITY_EDITOR
+        [UnityEditor.MenuItem("Tools/Scene-LogicConstructor/Add Logic Placer")]
+#endif
         public static void Create()
         {
+#if UNITY_EDITOR
             var find = Object.FindObjectOfType<LogicPlacer>();
              
             var placer = find != null ? find : new GameObject("Logic-Constructor").AddComponent<LogicPlacer>();
@@ -17,9 +19,10 @@ namespace Scene_LogicConstructor.Editor
             if (placer.IsNull)
             {
                 Debug.LogWarning("Please select any logic constructor template");
-                EditorUtility.OpenPropertyEditor(placer);
+                UnityEditor.EditorUtility.OpenPropertyEditor(placer);
             }
-            else Selection.activeGameObject = placer.gameObject;
+            else UnityEditor.Selection.activeGameObject = placer.gameObject;
+#endif
         }
     }
 }
